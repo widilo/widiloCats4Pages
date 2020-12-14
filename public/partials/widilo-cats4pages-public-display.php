2f0b269f29c7248b86d2374c4fe98ce6985e8baf
widilo <widilo@users.noreply.github.com>
2020-12-14 22:53:46 +0100

    Add public folder via file upload

---
+++ public/partials/widilo-cats4pages-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://muennecke-vollmers.de
+ * @since      1.0.0
+ *
+ * @package    Widilo_Cats4pages
+ * @subpackage Widilo_Cats4pages/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
